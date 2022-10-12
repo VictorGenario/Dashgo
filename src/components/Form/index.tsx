3e7b1aa34c43b92b1c264fb18bde62308d793a74
VictorGenario <victordamatagenario@gmail.com>
2022-10-12 18:16:25 -0300

    Quarto commit

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,14 +1,15 @@
 import { FormControl, FormLabel, Input as ChakraIput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
 } 
 
-export function Input({name, label, ...rest}: InputProps){
+export const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> =({name, label, ...rest}, ref) => {
   return(
     <FormControl>
-    <FormLabel htmlFor='email'>E-mail</FormLabel>
+     {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>} 
   
      <ChakraIput 
        name={name} 
@@ -20,8 +21,11 @@ export function Input({name, label, ...rest}: InputProps){
          bgColor: 'gray.900',
        }}
        size="lg"
+       ref={ref}
        {...rest}
       />
     </FormControl>
   );
 }
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
-import { Button, Flex, Stack } from '@chakra-ui/react'
-import { Input } from '../components/Form'
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Input } from '../components/Form';
 
+type SingInFormData = {
+  email: string;
+  password: string;
+}
 
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const  handleSignIn: SubmitHandler<SingInFormData> = async (values) =>{
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values)
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -16,18 +29,34 @@ export default function Home() {
         bg="gray.800" 
         p="8" 
         borderRadius={8} 
-        flexDirection="column">
+        flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
+        >
         
       <Stack spacing="4">
-        <Input name="email" type="email" label="E-mail" />
-        <Input name="password" type="passaword" label="Senha" />
+        <Input 
+          name="email"
+          type="email" 
+          label="E-mail"
+          {...register('email')} 
+        />
+
+        <Input 
+        name="passaword" 
+        type="password" 
+        label="Senha"
+        {...register('passaword')} 
+        />
+
       </Stack>
 
       
         <Button 
           type='submit' 
           mt="6" 
-          colorScheme="pink">
+          colorScheme="pink"
+          isLoading={formState.isSubmitting}
+          >
             Entrar
         </Button>
       </Flex>
